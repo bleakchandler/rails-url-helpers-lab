ee2031d97ce0f2927151c3f7c894fbd8b8213dcf
Bleak Chandler <bleakchandler@gmail.com>
2021-02-25 23:28:14 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  
+  #Index:
+  get "students", to: "students#index", as: "students"
+
+
+  #Show:
+  get "/students/:id", to: "students#show", as: "student"
+
+
+  #Edit:
+  get "/students/:id/activate", to: "students#activate", as: "activate_student"
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+bleak = Student.create(first_name: "Bleak", last_name: "Chandler")
+
+
+
+puts "Seeded!"
